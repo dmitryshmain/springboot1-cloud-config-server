f95e11cb624f3d45d2e5696a3d99c6780cc9f98c
Dmitry Shmain <dmitry.shmain@gmail.com>
2018-03-29 15:20:20 +0300

    c

--- src/main/java/com/dataart/dshmain/boottest1/BootTest1CloudConfigServerApplication.java
+++ src/main/java/com/dataart/dshmain/boottest1/BootTest1CloudConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.dataart.dshmain.boottest1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class BootTest1CloudConfigServerApplication {
 
 	public static void main(String[] args) {
